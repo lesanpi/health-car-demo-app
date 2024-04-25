4f43f2f4709f9964387ceb2c1cfe9689c5cf14ee
Luis <lespinerua@gmail.com>
2024-04-25 13:42:31 -0400

    feat: first commit 🚗

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.health_car_demo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:health_car_demo_app/app/app.dart';
+import 'package:health_car_demo_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
